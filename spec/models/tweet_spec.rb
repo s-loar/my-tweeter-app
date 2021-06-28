f916c84bddcec7749e25cc5f81196855e8be15fd
Steven Loar <sjl7909@gmail.com>
2021-06-28 13:21:46 -0400

    Added 2 more expectations

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -4,13 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
   describe '#body' do
+    let(:tweet) { build(:tweet) }
 
-    context 'with a body present' do
-      let(:tweet) { build(:tweet) }
-      it do
-        is_expected.to validate_presence_of :body
-        is_expected.not_to allow_value(nil).for(:body)
-      end
+    it do
+      is_expected.to validate_presence_of :body
+      is_expected.to allow_value('tweet body').for(:body)
+      is_expected.not_to allow_value(nil).for(:body)
     end
   end
 end
