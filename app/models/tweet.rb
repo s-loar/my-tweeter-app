301c3ff63164a3e105d1aa72537b358dc69cbc3d
Steven Loar <sjl7909@gmail.com>
2021-07-07 16:56:35 -0400

    Delete working.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -10,6 +10,17 @@ class Tweet < ApplicationRecord
   scope :deleted, -> { with_deleted.where.not(deleted_at: nil) }
 
   after_create_commit { broadcast_prepend_to "tweets" }
-  after_update_commit { broadcast_replace_to "tweets" }
+  after_update_commit { after_update_action }
   after_destroy_commit { broadcast_remove_to "tweets" }
+
+  private
+
+  def after_update_action
+    if self.deleted_at.blank?
+      broadcast_replace_to "tweets" 
+      return 
+    end
+
+    broadcast_remove_to "tweets"
+  end
 end
